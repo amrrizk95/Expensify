da132f39d1c7e51438dec888e36530b416d71cd1
amrrizk95 <amrelseady65@gmail.com>
2020-03-12 17:41:40 +0200

    initalCommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import {Provider} from 'react-redux'
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill',amount:4500 }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+//store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+class App extends Component {
+
+
+  render() {
+ 
+    return (
+      <div className="App">
+        <Provider store={store}>
+          <AppRouter/>
+        </Provider>
+
+  
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+// ReactDOM.render(<AppRouter />, document.getElementById('app'));
